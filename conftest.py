ee404590359f1c57d97df4a70d19e9b7e25b2f68
NickJacobsonUA <mykyta.yakovenko.eu@gmail.com>
2023-10-05 11:31:05 +0300

    conftest + requirements

--- conftest.py
+++ conftest.py
@@ -1,17 +1,28 @@
 # Fixtures - функции, которые позволяют делать что то до или после теста
 import pytest
+import time
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
+
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 # 1
-@pytest.fixture(scope="function",)                   # function - для открытия браузера в set up и закрывался в teardown каждого теста. Независимость теста.
-def driver():                           # открытие и закрытие функции. Для того что бы сделать функцию фикстурой прописывается декоратор сверху(смотреть выше)
-                                            # driver = webdriver.Chrome #webdriver берётся из selenium. также нужно скачать драйвер менеджер - использовать спец. бтблиотеку webdriver manager
-                                            # подключаем вэб драйвер мэнеджер через настройки и подключаем его через
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))  # импортируем бибилиотеку ChromeDriverManager() для открытия либо же запуска браузера
+@pytest.fixture(scope="function",)
+# function - для открытия браузера в set up и закрывался в teardown каждого теста. Независимость теста.
+def driver():
+    # открытие и закрытие функции. Для того что бы сделать функцию фикстурой прописывается декоратор сверху(смотреть выше)
+    # driver = webdriver.Chrome #webdriver берётся из selenium. также нужно скачать драйвер менеджер - использовать спец. бтблиотеку webdriver manager
+    # подключаем вэб драйвер мэнеджер через настройки и подключаем его через
+
+    #выбор юзер по умолчанию при открытии браузера
+    #options = webdriver.ChromeOptions()
+    #options.add_argument("--profile-directory=Profile1")
+    #options.add_argument("--user-data-dir=C:\\Users\\starl\\AppData\\Local\\Google\\Chrome\\User Data\\")
+
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))# импортируем бибилиотеку ChromeDriverManager() для открытия либо же запуска браузера
     driver.maximize_window()  # открытие браузера на весь экран
     yield driver
     driver.quit()  # закрытие браузера после завершения теста
-    # После того как мы создали фикстуру мы переходим к созданию базовой страницы base_page.py
+    # После того как создали фикстуру переходим к созданию базовой страницы base_page.py
